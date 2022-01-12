4c63c35bc8c6892c5f78f0782aaa5cb6f764421a
Tal Zaken <talzaken@gmail.com>
2022-01-12 18:22:01 -0500

    removed entries from call to generator, as it now is produced within the generator

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, Response, render_template, request
 from frames_generator import screenshots
-from wikipedia import get_entries, get_random_title
+from wikipedia import get_random_title
 
 app = Flask(__name__)
 
@@ -15,16 +15,13 @@ def index():
 
 @app.route('/random', methods=["GET"])
 def random_wiki_feed():
-    #title = get_random_title()
-    #entries = get_entries(title)[:10]
     return Response(screenshots(get_random_title),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/wiki_feed/<title>')
 def wiki_feed(title):
-    entries = get_entries(title)
-    return Response(screenshots(title, entries),
+    return Response(screenshots(title),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
